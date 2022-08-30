0136e861fe8fc1ed7325eace29b0ad8491c21418
Haritha <harithareddi05@gmail.com>
2022-08-30 08:41:48 +0000

    NOTIFICATION

---
+++ index.js
@@ -0,0 +1,12 @@
+const Notification = (props) => {
+  //  Write your code here.
+};
+
+const element = (
+  //  Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Notifications</h1>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
